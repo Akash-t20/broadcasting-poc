48fd117c63f5ff092c5b42bf7d7d928ce49155f1
Akash-t20 <akash.t@protco.in>
2025-02-22 10:54:39 +0530

    ON/OFF changes

--- android/app/src/main/java/com/nativelocalstorage/NativeLocalStorageModule.java
+++ android/app/src/main/java/com/nativelocalstorage/NativeLocalStorageModule.java
@@ -1,5 +1,4 @@
 package com.nativelocalstorage;
-import static androidx.core.app.ActivityCompat.*;
 
 import static com.facebook.common.util.Hex.hexStringToByteArray;
 
@@ -11,52 +10,28 @@ import android.bluetooth.le.AdvertisingSet;
 import android.bluetooth.le.AdvertisingSetCallback;
 import android.bluetooth.le.AdvertisingSetParameters;
 import android.bluetooth.le.BluetoothLeAdvertiser;
-import android.bluetooth.le.PeriodicAdvertisingParameters;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.ParcelUuid;
 import android.util.Log;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
-import java.util.UUID;
-
 public class NativeLocalStorageModule extends ReactContextBaseJavaModule {
 
 
     private BluetoothLeAdvertiser advertiser;
-    private AdvertisingSet currentAdvertisingSet;//.....
+    private AdvertisingSet currentAdvertisingSet;
     private AdvertisingSetCallback advertisingCallback;
 
     private static final String TAG = "NativeLocalStorage";
     private static final String TAG2 = "Protco Broadcaster";
 
-    private static final String PREF_NAME = "BluetoothPrefs";
 
     public static  String groupId;
-
-
-//
-//    private BluetoothLeScanner bluetoothLeScanner;
-//    private List<BluetoothDevice> scannedDevices = new ArrayList<>();
-//    private ScanCallback scanCallback;
-//    private boolean isScanning = false;
-//
-//
-
     public NativeLocalStorageModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -75,7 +50,8 @@ public class NativeLocalStorageModule extends ReactContextBaseJavaModule {
         promise.resolve(true); // asynchronous operations-> promise.resolve(), the Promise's state changes from "pending" to "fulfilled"
         try {
             Log.d(TAG2,"Starting broadcasting");
-            BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter(); //BluetoothAdapter is your primary way to interact with Bluetooth functionality on Android, if the device doesn't have Bluetooth  it will  return null.
+            BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+            //BluetoothAdapter is your primary way to interact with Bluetooth functionality on Android, if the device doesn't have Bluetooth  it will  return null.
 
             if (adapter == null) {
                 promise.reject("BLUETOOTH_UNAVAILABLE", "Bluetooth adapter not available");
@@ -90,52 +66,31 @@ public class NativeLocalStorageModule extends ReactContextBaseJavaModule {
                 promise.reject("ADVERTISING_UNSUPPORTED", "Bluetooth LE advertising not supported");
                 return;
             }
-           //The 2M PHY is a feature that allows for higher data transfer speeds (2 Mbps) compared to the original 1M PHY (1 Mbps)
-            if (!adapter.isLe2MPhySupported()) {
-                promise.reject("2M_PHY_UNSUPPORTED", "2M PHY not supported!");
-                return;
-            }
-
-             //introduced in Bluetooth 5-> large amount data, && for for periodic advertising and other advanced advertising features.
-            if (!adapter.isLeExtendedAdvertisingSupported()) {
-                promise.reject("EXTENDED_ADVERTISING_UNSUPPORTED", "Extended Advertising not supported!");
-                return;
-            }
-
 
 
             int maxDataLength = 0;
-            maxDataLength = adapter.getLeMaximumAdvertisingDataLength();// This length includes all data in the advertisement, such as flags, manufacturer data, service UUIDs, service data,
+            maxDataLength = adapter.getLeMaximumAdvertisingDataLength(); // This length includes all data in the advertisement, such as flags, manufacturer data, service UUIDs, service data,
             Log.i(TAG, "Max advertising data length: " + maxDataLength);
 
             AdvertisingSetParameters parameters = getAdvertisingSetParameters();
 
             // Manufacturer Specific Data
-            int manufacturerId = 0x5450; //change id as wanted
-            String hexData = "A39501FFA5AF00000101";
-            byte[] manufacturerData = hexStringToByteArray(hexData);  // change data required for u bro ; getbyte -> changes string to byte
-//            byte[] manufacturerData = new byte[2 + actualData.length];
+            int manufacturerId = 0x5450; //change ID
+            String hexData = "A39501FFA5AF00000101"; // change Data
+            byte[] manufacturerData = hexStringToByteArray(hexData);
 
-
-
-//            manufacturerData[0] = (byte) (0x12); // Group ID byte 1
-//            manufacturerData[1] = (byte) (0x34); // Group ID byte 2
-//            System.arraycopy(actualData, 0, manufacturerData, 2, actualData.length);
-
-
-
+            //For service Data
             // UUID myServiceUUID = UUID.fromString("cb7d58a6-854e-4ffa-872e-251a1b02e164"); // uuid changes
 
             AdvertiseData data = new AdvertiseData.Builder()
-                    //.addServiceData(new ParcelUuid(myServiceUUID), "SWITCH-ON".getBytes())
-                    .setIncludeDeviceName(true) // identify your device in scans.
+
+                    //.addServiceData(new ParcelUuid(myServiceUUID), "SWITCH-ON".getBytes())  // for service Data
+
+                    .setIncludeDeviceName(true)
                     .addManufacturerData(manufacturerId, manufacturerData)
                     .build();
-            AdvertiseData scanResponse = new AdvertiseData.Builder().build();
-            PeriodicAdvertisingParameters periodicAdvertisingParameters=null;
 
 
-           // boolean isPermissionPresent = checkSelfPermission(this.getReactApplicationContext(), Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED;
             advertisingCallback = new AdvertisingSetCallback() {
                 @Override
                 public void onAdvertisingSetStarted(AdvertisingSet advertisingSet, int txPower, int status) {
@@ -143,7 +98,14 @@ public class NativeLocalStorageModule extends ReactContextBaseJavaModule {
                     Log.i(TAG2, "Protco Broadcasting Started: txPower=" + data.toString());
                     if (status == AdvertisingSetCallback.ADVERTISE_SUCCESS) {
                         currentAdvertisingSet = advertisingSet;
+
+                        // Adding handler for to stop broadcasting in 3 sec (change time as u want)
+                        new android.os.Handler(android.os.Looper.getMainLooper()).postDelayed(() -> {
+                            stopAdvertising(promise);
+                        }, 10000);
+
                         promise.resolve(null);
+
                     } else {
                         promise.reject("ADVERTISING_START_FAILED", "Advertising start failed: " + status);
                         Log.e(TAG2, "Advertising start failed: " + status);
@@ -155,16 +117,16 @@ public class NativeLocalStorageModule extends ReactContextBaseJavaModule {
                     Log.i(TAG2, "Advertising stopped");
                     currentAdvertisingSet = null;
                 }
-
             };
+
             if (ActivityCompat.checkSelfPermission(this.getReactApplicationContext(), Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
 
                 return;
             }
             advertiser.startAdvertisingSet(parameters,data,null,null,null, advertisingCallback);
-
             Log.e(TAG2, "Method called -> startAdvertisingSet ");
             return;
+
         } catch (Exception e) {
             promise.reject("ERROR", "Error starting advertising: " + e.getMessage());
             Log.e(TAG, "Error in startAdvertising", e);
@@ -174,58 +136,51 @@ public class NativeLocalStorageModule extends ReactContextBaseJavaModule {
         AdvertisingSetParameters parameters = null;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             parameters = new AdvertisingSetParameters.Builder()
-                    .setLegacyMode(false)
+                    .setLegacyMode(false) // for legacy mode
                     .setConnectable(false) // You only need to broadcast data && You don't need two-way communication.
                     .setInterval(AdvertisingSetParameters.INTERVAL_LOW)//  your device will advertise very frequently && making it easier and faster for other devices to discover it.
                     .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_MEDIUM)
-                    .setPrimaryPhy(BluetoothDevice.PHY_LE_1M)
-                    .setSecondaryPhy(BluetoothDevice.PHY_LE_2M)
                     .build();
         }
         return parameters;
     }
 
-
-
-
-//    @ReactMethod
-//    public void startScanning(Promise promise) {
-//        if (ContextCompat.checkSelfPermission(getReactApplicationContext(), Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
-//                ContextCompat.checkSelfPermission(getReactApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            promise.reject("PERMISSION_DENIED", "Bluetooth scan and location permissions are not granted.");
-//            return;
-//        }
-//
-//        if (isScanning) {
-//            promise.reject("SCANNING_ALREADY_IN_PROGRESS", "Scanning is already in progress.");
-//            return;
-//        }
-//
-
-
-
-
-
-
     @ReactMethod
     public void stopAdvertising(Promise promise) {
         promise.resolve(true);
+
         if (advertiser != null && advertisingCallback != null
                 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
             if (ActivityCompat.checkSelfPermission(this.getReactApplicationContext(), Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
-
                 return;
             }
-            advertiser.stopAdvertisingSet(advertisingCallback);
-            currentAdvertisingSet = null;
-            Log.i(TAG, "Advertising stopped successfully");
-        } else {
-            Log.e(TAG, "Cannot stop advertising, advertiser or callback is null");
+           // OFF data here passed
+            if (currentAdvertisingSet!=null){
+                int manufacturerId = 0x5450; // Same manufacturer ID
+                String offHexData = "A39501FF6E7800000102"; // OFF data
+                byte[] offData = hexStringToByteArray(offHexData);
+
+                AdvertiseData offAdvertiseData = new AdvertiseData.Builder()
+                        .addManufacturerData(manufacturerId, offData)
+                        .build();
+
+                currentAdvertisingSet.setAdvertisingData(offAdvertiseData);
+                Log.i(TAG, "Passing  OFF data before stopping advertising");
+
+                 //handler for stop , as after passing command then after how many second u want to light off
+                new android.os.Handler(android.os.Looper.getMainLooper()).postDelayed(() -> {
+                    advertiser.stopAdvertisingSet(advertisingCallback);
+                    currentAdvertisingSet = null;
+                    Log.i(TAG, "Advertising stopped  after OFF data");
+                }, 1000); // change time
+
+            } else {
+                advertiser.stopAdvertisingSet(advertisingCallback);
+                currentAdvertisingSet = null;
+                Log.i(TAG, "Advertising stopped successfully without OFF data");
+            }
         }
-
-
-
     }
 
 }
